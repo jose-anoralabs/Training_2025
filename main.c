de3025a39f9a97109300386f11f289df627718b9
AIPLBLRNT009\Santosh Kumar S <Santoshkumar.S@anorasolutions.com>
2025-04-22 12:04:05 +0530

    Main function of stack operation is added

---
+++ main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#define SIZE 100
+void push(int);
+void pop();
+void peek();
+void display();
+int stack[SIZE];
+int top = -1;
+
+int main() {
+    int choice, value;
+    while (1) {
+        printf("\n--- STACK MENU ---\n");
+        printf("WARNING: SIZE OF STACK IS 100");
+        printf("1. PUSH\n2. POP\n3. PEEK\n4. DISPLAY\n5. EXIT\n");
+        printf("ENTER YOUR CHOICE: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+        case 1:
+            printf("ENTER VALUE TO BE PUSHED: ");
+            scanf("%d", &value);
+            push(value);
+            break;
+        case 2:
+            pop();
+            break;
+        case 3:
+            peek();
+            break;
+        case 4:
+            display();
+            break;
+        case 5:
+            printf("EXIT SUCCESS\n");
+            return 0;
+        default:
+            printf("INVALID CHOICE!!! TRY AGAIN!!!\n");
+        }
+    }
+}
